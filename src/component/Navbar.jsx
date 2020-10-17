4c946c509450d3838b724a30686dd30b5bf1fd91
SeoHyeonKim <marvelopers@gmail.com>
2020-10-17 23:57:39 +0900

    Update Navbar.jsx

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,29 +1,41 @@
 import React from 'react';
-import '../css/Navbar.css';
-import logo from '../imgs/blog_logo.png';
-
+import styled from "styled-components"
 
 function Navbar() {
 	return (
-		<nav id="navbar">
-			<div class="navbar__container">
+		<StyledNavbar>
+			<NavbarContainer>
 				<div class="navbar__logo">
 					<a href="index.html">
-						<img src={logo} alt="로고" class="navbar__logo--img" />
+						<img src="" alt="로고" class="navbar__logo--img" />
 					</a>
 				</div>
 				<div class="navbar__menu">
 					<ul class="navbar__menu--list">
-						<li data-link="#aboutMe" class="navbar__menu__item active">About Me</li>
 						<li data-link="#goToBlog" class="navbar__menu__item"> <a href="blog.html">Go to blog</a></li>
+						<li data-link="#aboutMe" class="navbar__menu__item active">About Me</li>
 					</ul>
 				</div>
-				<button id="toggleBtn" class="navbar__toggle-btn">
-					<i class="fas fa-bars"></i>
-				</button>
-			</div>
-		</nav>
+			</NavbarContainer>
+		</StyledNavbar>
 	);
 }
 
 export default Navbar;
+
+const StyledNavbar = styled.nav`
+  position: fixed;
+  width: 100%;
+  z-index: 1000;
+`;
+
+const NavbarContainer = styled.div`
+  width: 1080px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  background-color: transparent;
+  align-items: center;
+  color: var(--color-dark-black);
+  padding: 3px;
+	`;
