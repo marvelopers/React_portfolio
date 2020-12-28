0b1db2340a774461c850be12069ec8b28c544c50
marvelopers <marvelopers@gmail.com>
2020-12-29 00:09:24 +0900

    DEL

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { store } from "./store/store";
 
 
 
@@ -14,5 +14,4 @@ ReactDOM.render(
 );
 
 
-serviceWorker.unregister();
 
